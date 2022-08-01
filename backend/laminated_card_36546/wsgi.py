b461c56bf57fa6207d2afce21515396d87657933
Crowdbotics <team@crowdbotics.com>
2022-08-01 21:13:30 +0000

    Initial commit.

---
+++ backend/laminated_card_36546/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for laminated_card_36546 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laminated_card_36546.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "laminated_card_36546",
+  url: "https://laminated_card_36546.botics.co",
+  api: "https://laminated_card_36546.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
